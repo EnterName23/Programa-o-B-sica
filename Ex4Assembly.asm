5dccbd4c8623c9610599307fd697f5bf8afe1586
EnterName23 <168845529+EnterName23@users.noreply.github.com>
2024-08-19 11:26:51 -0300

    Ex4Assembly.asm

---
+++ Ex4Assembly.asm
@@ -0,0 +1,13 @@
+MOV AX, 4000h     ; Define pra 4000h
+MOV DS, AX        ; Carrega no segmento de dados
+MOV BX, 00h
+MOV CX, 0FFFFh    ; Casa final
+MOV AL, 0AAh      ; Valor a ser gravado
+
+memoria:     
+    MOV DS:[BX], AL  ; Armazena o valor em [BX]
+    ADD BX, 01h
+    LOOP memoria ; Vai reduzindo o CX até 0
+
+MOV AH, 4Ch       ; Código para terminar o programa
+INT 21h           ; Chama o DOS para terminar o programa
